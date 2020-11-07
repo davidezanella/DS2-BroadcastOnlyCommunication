6c1846d8e6e8df778ed1904703a6118f6e12cb97
simonedegiacomi <simonedegiacomi97@gmail.com>
2020-11-07 22:15:41 +0200

    use primitive types instead of Boxed

--- src/broadcastOnlyCommunication/Relay.java
+++ src/broadcastOnlyCommunication/Relay.java
@@ -1,17 +1,14 @@
 package broadcastOnlyCommunication;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 import repast.simphony.engine.schedule.ScheduledMethod;
-import repast.simphony.essentials.RepastEssentials;
 import repast.simphony.query.space.grid.GridCell;
 import repast.simphony.query.space.grid.GridCellNgh;
 import repast.simphony.space.continuous.ContinuousSpace;
 import repast.simphony.space.grid.Grid;
 import repast.simphony.space.grid.GridPoint;
-import repast.simphony.util.collections.Pair;
 
 public abstract class Relay {
 	protected ContinuousSpace<Object> space;
@@ -49,21 +46,21 @@ public abstract class Relay {
 		GridPoint pt = grid.getLocation(this);
 
 		// get all the Relays in the grid
-		Integer extentX = grid.getDimensions().getWidth() / 2;
-		Integer extentY = grid.getDimensions().getHeight() / 2;
+		int extentX = grid.getDimensions().getWidth() / 2;
+		int extentY = grid.getDimensions().getHeight() / 2;
 		GridCellNgh<Relay> nghCreator = new GridCellNgh<Relay>(grid, pt, Relay.class, extentX, extentY);
 		List<GridCell<Relay>> gridCells = nghCreator.getNeighborhood(true);
 
 		for (GridCell<Relay> cell : gridCells) {
 			for (Relay relay : cell.items()) {
-				Double distance = Utils.distanceBetweenPoints(pt, grid.getLocation(relay));
-				Integer missingTicks = (int) Math.ceil(distance);
+				double distance = Utils.distanceBetweenPoints(pt, grid.getLocation(relay));
+				int missingTicks = (int) Math.ceil(distance);
 				relay.onSense(p, missingTicks);
 			}
 		}
 	}
 
-	public Integer nextRef(Perturbation p) {
+	public int nextRef(Perturbation p) {
 		return p.ref + 1; // for sequence numbers
 		// return hash(P) // for hash chaining
 	}
--- src/broadcastOnlyCommunication/Station.java
+++ src/broadcastOnlyCommunication/Station.java
@@ -9,13 +9,12 @@ import repast.simphony.query.space.grid.GridCell;
 import repast.simphony.query.space.grid.GridCellNgh;
 import repast.simphony.space.continuous.ContinuousSpace;
 import repast.simphony.space.grid.Grid;
-import repast.simphony.space.grid.GridPoint;
 
 public class Station {
 	private ContinuousSpace<Object> space;
 	private Grid<Object> grid;
 	private String ID = "station";
-	private Integer ref = 0;
+	private int ref = 0;
 
 	public Station(ContinuousSpace<Object> space, Grid<Object> grid) {
 		this.space = space;
@@ -25,12 +24,12 @@ public class Station {
 	@ScheduledMethod(start = 1, interval = 100)
 	public void sendPerturbation() {
 		// get the grid location of the Station
-		GridPoint pt = grid.getLocation(this);
+		var pt = grid.getLocation(this);
 
 		// get all the Relays in the grid
-		Integer extentX = grid.getDimensions().getWidth() / 2;
-		Integer extentY = grid.getDimensions().getHeight() / 2;
-		GridCellNgh<Relay> nghCreator = new GridCellNgh<Relay>(grid, pt, Relay.class, extentX, extentY);
+		var extentX = grid.getDimensions().getWidth() / 2;
+		var extentY = grid.getDimensions().getHeight() / 2;
+		var nghCreator = new GridCellNgh<Relay>(grid, pt, Relay.class, extentX, extentY);
 		List<GridCell<Relay>> gridCells = nghCreator.getNeighborhood(true);
 
 		String value = UUID.randomUUID().toString();
