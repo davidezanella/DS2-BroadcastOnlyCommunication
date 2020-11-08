00a7b491c2e734c0136e916c280f5450e746d433
texdade <davidetessarolo@gmail.com>
2020-11-08 16:28:51 +0100

    Implemented RelayII

---
+++ src/broadcastOnlyCommunication/RelayII.java
@@ -0,0 +1,64 @@
+package broadcastOnlyCommunication;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import repast.simphony.essentials.RepastEssentials;
+import repast.simphony.space.continuous.ContinuousSpace;
+import repast.simphony.space.grid.Grid;
+import repast.simphony.space.grid.GridPoint;
+
+public class RelayII extends Relay {
+	private Map<String, Integer> frontier = new HashMap<String, Integer>();
+	private ArrayList<Perturbation> bag = new ArrayList<Perturbation>();
+
+	public RelayII(ContinuousSpace<Object> space, Grid<Object> grid, String id) {
+		super(space, grid, id);
+	}
+
+	public void processPerturbation(Perturbation p) {
+		GridPoint pt = grid.getLocation(this);
+		Double tick = RepastEssentials.GetTickCount();
+		Boolean noElementsToDeliver = false;
+		System.out.println(tick + " -- Relay " + pt.getX() + " - " + pt.getY() + ": " + p.val);
+
+		if (!frontier.containsKey(p.src)) {
+			frontier.put(p.src, 0);
+		}
+
+		//if the perturbation has a ref number higher than expected, put it in the bag
+		//because it is getting an OOO delivery
+		if (p.ref >= frontier.get(p.src) && !bag.contains(p)) {
+			//add the new perturbation to the bag, even if it can be forwarded immediately
+			bag.add(p); 
+			
+			//cycle through the list to find out if one or more elements can be forwarded
+			while(!noElementsToDeliver) {
+				noElementsToDeliver = true;
+				
+				for(int i=0; i<bag.size(); i++) {
+					Perturbation bagPt = bag.get(i);
+					
+					//an element can be forwarded
+					if(frontier.get(bagPt.src).equals(bagPt.ref)) {
+						forwardPerturbation(bagPt);
+						frontier.put(bagPt.src, nextRef(bagPt));
+						bag.remove(bagPt);
+						//it could cause something else to be forwarded too
+						noElementsToDeliver = false;
+					}
+				}
+			}
+		}	
+		
+		
+			
+		
+		forwardPerturbation(p);
+		frontier.put(p.src, nextRef(p));
+		// observer notification of P.val goes here
+
+		
+	}
+}
