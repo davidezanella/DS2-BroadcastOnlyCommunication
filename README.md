4d3ba8ab97dbcaa2cbc6b991594c56c21d3e88f9
texdade <davidetessarolo@gmail.com>
2020-11-24 10:15:11 +0100

    Uploading README and updated (but not complete) version of script

---
+++ README.md
@@ -0,0 +1,34 @@
+# Broadcast-Only Communication Model Based on Replicated Append-Only Logs
+This project implements the broadcast-only communication model based on replicated append-only logs, as specified in the [assigned paper](https://bucchiarone.bitbucket.io/papers/acmdl19-295.pdf)
+
+## Requirements
+* Java 11
+* Eclipse IDE 2020-06 (4.16.0)
+* Repast Symphony 2.8.0
+
+Optionally, for running the plotting scripts:
+* Python 3
+
+## Installation
+1. Clone the repository
+2. Import the project as Repast Project into Eclipse
+3. Run the simulator
+
+## Simulator parameters
+* Maximum number of relays: the maximum number of relays that can be present on a given run
+* Minimum number of relays: the minimum number of relays that can be present on a given run
+* New relay probability: the probability that a new relay will be spawned in a period of 100 ticks
+* Relay crash probability: the probability that a new relay will be deleted in a period of 100 ticks
+* Number of relays: initial number of relays
+* Number of stations: initial number of stations
+* Packet size: size of packets sent via perturbations
+* Transmission speed: available bandwidth per tick
+* Perturbation drop probability: probability that a perturbation is dropped
+* Protocol version: defines which protocol version (from those presented in the paper) the simulator will run
+* Stop at: the simulation will stop once it reaches the indicated amount of ticks
+
+## Plotting script parameters
+
+
+## Reference
+Christian F. Tschudin: Broadcast-Only Communication Model Based on Replicated Append-Only Logs, ACM SIGCOMM Computer Communication Review, Volume 49 Issue 2, April 2019
--- graphScripts/create_plots.py
+++ graphScripts/create_plots.py
@@ -1,4 +1,5 @@
 import csv
+import os
 import sys
 import argparse
 import matplotlib.pyplot as plt 
@@ -6,9 +7,11 @@ import statistics
 
 '''
 This script has two run modes:
-(1)Plotting latencies - If the print_only_to param isn't provided, the script draw the graph of the latency
+(1)Plotting latencies - Input [Stations logs, relays logs]
+   If the print_only_to param isn't provided, the script draw the graph of the latency
    of each perturbation of a single simulation
-(2)Calculating latency - the script will append to a file the mean latency of a run, in the format
+(2)Calculating latency - Input [Batch stations logs, batch relays logs, kind of sim, path to csv file with the latencies]
+   The script will append to a file the mean latency of a run, in the format
    [RunType - latency], for future use with another script 
 '''
 
@@ -20,85 +23,103 @@ def parse_arg(argv):
     parser.add_argument('--print_only_to', type=str, help='Path to print the latency result')
     return parser.parse_args(argv)
 
+def read_stations(stations_path):
+    file_station = open(str(stations_path))
+    csv_station = csv.DictReader(file_station, delimiter=',')
 
-args = parse_arg(sys.argv[1:])
+    stations = {}
 
-file_station = open(str(args.stations))
-csv_station = csv.DictReader(file_station, delimiter=',')
-
-file_relay = open(str(args.relays))
-csv_relay = csv.DictReader(file_relay, delimiter=',')
-
-stations = {}
-relays = {}
-
-#create a dict stations[station_id: [<tick, sent_ref, value>]]
-for row in csv_station:
-    station_id = row['StationId']
-    if station_id not in stations.keys():
-        stations[station_id] = []
-    if row['NewPerturbationValue'] != "":
-        stations[station_id].append({
-            'station': row['StationId'],
-            'tick': float(row['tick']),
-            'sent_ref': int(row['CurrentRef'])-1,
-            'value': row['NewPerturbationValue']
-        })
-
-
-#create a dict relays[relay_id: [<tick, station, ref>]]
-for row in csv_relay:
-    relay_id = row['RelayId']
-    if relay_id not in relays.keys():
-        relays[relay_id] = []
-    if row['ArrivedPerturbations'] != "":
-        perturbations = row['ArrivedPerturbations'].split(',')
-        for pert in perturbations:
-            splitted = list(filter(None, pert.split(' ')))
-            relays[relay_id].append({
+    #create a dict stations[station_id: [<tick, sent_ref, value>]]
+    for row in csv_station:
+        station_id = row['StationId']
+        if station_id not in stations.keys():
+            stations[station_id] = []
+        if row['NewPerturbationValue'] != "":
+            stations[station_id].append({
+                'station': row['StationId'],
                 'tick': float(row['tick']),
-                'station': splitted[0],
-                'ref': int(splitted[1])
+                'sent_ref': int(row['CurrentRef'])-1,
+                'value': row['NewPerturbationValue']
             })
 
-perturbations = {}
+    return stations
 
-#plot mean latency per perturbation
+def read_stations_batch(stations_path):
+    file_station = open(str(stations_path))
+    csv_station = csv.DictReader(file_station, delimiter=',')
 
-for station in stations:
-    for pert in stations[station]:
-        perturbations[pert["station"], pert["sent_ref"]]=[pert["tick"], [], pert["value"]]
+    run_stations = {}
 
-print("Perturbations received by relays")
-#make a list of all sent perturbations
-for relay in relays:
-    #arr_p means arrived_perturbation, it is not pirate language
-    for arr_p in relays[relay]:
-        perturbations[arr_p["station"], arr_p["ref"]][1].append({
-            'relay': relay,
-            'arr_tick': arr_p["tick"]
-        })
-        print(perturbations[arr_p["station"], arr_p["ref"]])
+    #create a dict stations [run: [station_id: [<tick, sent_ref, value>]]]
+    for row in csv_station:
+        run_id = row['run']
+        station_id = row['StationId']
+        if run_id not in run_stations.keys():
+            run_stations[run_id] = {}
+        if station_id not in run_stations[run_id].keys():
+            run_stations[run_id][station_id] = []
+        if row['NewPerturbationValue'] != "":
+            run_stations[run_id][station_id].append({
+                'station': row['StationId'],
+                'tick': float(row['tick']),
+                'sent_ref': int(row['CurrentRef'])-1,
+                'value': row['NewPerturbationValue']
+            })
+    return run_stations
 
-#compute the mean latency for each pert and draw graph
-x = [] #x axis
-y = [] #y axis
+def read_relays(relays_path):
+    file_relay = open(str(relays_path))
+    csv_relay = csv.DictReader(file_relay, delimiter=',')
 
-i = 0
-for pert in perturbations:
-    x.append(str(pert))
-    count = 0
-    latency = 0
-    for data in perturbations[pert][1]:
-        count+=1
-        latency+=data['arr_tick']
+    relays = {}
+
+    #create a dict relays[relay_id: [<tick, station, ref>]]
+    for row in csv_relay:
+        relay_id = row['RelayId']
+        if relay_id not in relays.keys():
+            relays[relay_id] = []
+        if row['ArrivedPerturbations'] != "":
+            perturbations = row['ArrivedPerturbations'].split(',')
+            for pert in perturbations:
+                splitted = list(filter(None, pert.split(' ')))
+                relays[relay_id].append({
+                    'tick': float(row['tick']),
+                    'station': splitted[0],
+                    'ref': int(splitted[1])
+                })
+
+    return relays
+
+def read_relays_batch(relays_path):
+    file_relay = open(str(relays_path))
+    csv_relay = csv.DictReader(file_relay, delimiter=',')
+
+    run_relays = {}
+
+    #create a dict relays [run: [relay_id: [<tick, station, ref>]]]
+    for row in csv_relay:
+        run_id = row['run']
+        relay_id = row['RelayId']
+        if run_id not in run_relays.keys():
+            run_relays[run_id] = {}
+        if relay_id not in run_relays[run_id].keys():
+            run_relays[run_id][relay_id] = []
+        if row['ArrivedPerturbations'] != "":
+            perturbations = row['ArrivedPerturbations'].split(',')
+            for pert in perturbations:
+                splitted = list(filter(None, pert.split(' ')))
+                run_relays[run_id][relay_id].append({
+                    'tick': float(row['tick']),
+                    'station': splitted[0],
+                    'ref': int(splitted[1])
+                })
     
-    latency = latency / count
-    perturbations[pert][1] = latency
-    y.append(perturbations[pert][1] - perturbations[pert][0])
-    i+=1
+    return run_relays
 
-if(args.print_only_to == None):
+
+
+
+def draw_latency(x, y):
     # plotting the points  
     plt.plot(x, y) 
     
@@ -111,17 +132,75 @@ if(args.print_only_to == None):
     plt.title('Latency graph') 
     
     # function to show the plot 
-    plt.show() 
-else:
-    if(args.scenario != None):
+    plt.show()
+
+def main():
+    args = parse_arg(sys.argv[1:])
+
+    if(args.scenario == None and args.print_only_to == None):
+        #usage mode (1), plot from single run
+        perturbations = {}
+
+        stations = read_stations(args.stations)
+        relays = read_relays(args.relays)
+
+        #create a dict station, sent_ref: tick, [], value
+        for station in stations:
+            for pert in stations[station]:
+                perturbations[pert["station"], pert["sent_ref"]]=[pert["tick"], [], pert["value"]]
+
+        print("Perturbations received by relays")
+        #make a list of all sent perturbations
+        for relay in relays:
+            #arr_p means arrived_perturbation, it is not pirate language
+            for arr_p in relays[relay]:
+                perturbations[arr_p["station"], arr_p["ref"]][1].append({
+                    'relay': relay,
+                    'arr_tick': arr_p["tick"]
+                })
+                print(perturbations[arr_p["station"], arr_p["ref"]])
+
+        #compute the mean latency for each pert and draw graph
+        x = [] #x axis
+        y = [] #y axis
+
+        i = 0
+
+        #for each perturbation, calculate the mean latency
+        for pert in perturbations:
+            x.append(str(pert))
+            count = 0
+            latency = 0
+            for data in perturbations[pert][1]:
+                count+=1
+                latency+=data['arr_tick']
+            
+            latency = latency / count
+            perturbations[pert][1] = latency
+            y.append(perturbations[pert][1] - perturbations[pert][0])
+            i+=1
+
+        draw_latency(x,y)
+    elif(args.scenario != None and args.print_only_to == None):
+        #usage mode (2), load multiple scenario from batch log
+        stations = read_stations_batch(args.stations)
+        relays = read_relays_batch(args.relays)
+
+        #now that we have all data, compute the median latency for all runs
+        for run in stations:
+            
+
+
         row=str(args.scenario)+","+str(statistics.median(y))
 
         with open(args.print_only_to,'a') as fd:
             fd.write(row)
     else:
-        print("You have to specify a scenario type!")
+        print("Missing arguments!")
+        
 
-
+if __name__ == "__main__":
+    main()
 
 
 
