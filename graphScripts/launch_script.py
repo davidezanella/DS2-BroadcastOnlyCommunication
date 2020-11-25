492ac18fd0a67f18dda45da50d265b1a15888181
Davide <davide.zanella10@gmail.com>
2020-11-25 14:26:32 +0100

    Add (ugly) python script to easily plot simulation results

---
+++ graphScripts/launch_script.py
@@ -0,0 +1,27 @@
+from os import listdir, system
+from os.path import isfile
+
+
+protocol_folder_name = "2"
+
+output_path = '../output/'
+tmp_path = output_path + protocol_folder_name
+scenarios_dir = listdir(tmp_path)
+
+csv_filename = "test.csv"
+
+for scenario in scenarios_dir:
+    path = tmp_path + "/" + scenario + "/output/"
+    txt_files = listdir(path)
+    relay, station = '', ''
+    for txt in txt_files:
+        if 'OutputRelay' in txt and 'batch_param_map.' not in txt:
+            relay = path + '/' + txt
+        if 'OutputStation' in txt and 'batch_param_map.' not in txt:
+            station = path + '/' + txt
+
+    cmd = "python3 create_plots.py --relays {} --stations {} --scenario {} --print_only_to {}".format(relay, station, scenario, csv_filename)
+    system(cmd)
+
+cmd = "python3 plot_multiple_runs.py --input_csv {}".format(csv_filename)
+system(cmd)
